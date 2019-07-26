4fa420ac46c34810ff012495895125931697930e
Rian Mueller <rian@Rians-MacBook-Pro-2.local>
2019-07-25 14:34:57 -1000

    resolves all tests

--- smallest_multiple.js
+++ smallest_multiple.js
@@ -6,9 +6,18 @@
  * @return { Number }         Lowest Positive Number that is evenly divisible by all numbers
  *                            between 1 and `ceiling`
  */
-module.exports = function( ceiling ) {
+module.exports = function(ceiling) {
   // do work here
+  // Some optimizations:
+  // 1) The result must be divisible by 'ceiling' so the loop iterates through multiples of 'ceiling' rather than all integers
+  // 2) If a number is divisible by 20 then it is already divisible by 2, 4, 5, and 10, so checking these numbers is redundant. 18 also makes 9, 6, and 3 redundant, 16 makes 8 and 4 redundant, 15 makes 5 redundant, and 14 makes 7 redundant. So when the ceiling is 20 a number only has to be checked against 11-20, 1-10 are implicit. This should work as a general rule for all values of ceiling.
 
-
-  return 0;
+  let x = 1;
+  for (y = Math.floor(ceiling / 2) + 1; y <= ceiling - 1; y++) {
+    if ((x * ceiling) % y !== 0) {
+      x++;
+      y = 1;
+    }
+  }
+  return x * ceiling;
 };
